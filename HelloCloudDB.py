45774de89bab9cf08a02a9c7900a557e699e2124
KRPINF <korapin.infueng@gmail.com>
2021-01-26 10:16:41 +0700

    new01.py

--- HelloCloudDB.py
+++ HelloCloudDB.py
@@ -45,7 +45,7 @@ def get_staffs():
 # Web Root Hello
 @app.route('/', methods=['GET'])
 def get():
-    return jsonify({'ms': 'Hello Cloud DB1'})
+    return jsonify({'ms': 'Hello Cloud DB01'})
 # Run Server
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
