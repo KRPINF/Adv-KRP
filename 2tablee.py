f55e8c0485643eac07b90e3d5640057ff3e8b29f
KRPINF <korapin.infueng@gmail.com>
2021-02-23 11:23:15 +0700

    up2u

--- 2tablee.py
+++ 2tablee.py
@@ -15,34 +15,34 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 #Staff Class/Model
-class Staffs(db.Model):
-    id = db.Column(db.String(13), primary_key=True, unique=True)
+class KPOPs(db.Model):
+    id = db.Column(db.String(20), primary_key=True, unique=True)
     name = db.Column(db.String(50))
-    email = db.Column(db.String(25))
-    phone = db.Column(db.String(10))
+    type = db.Column(db.String(25))
+    company = db.Column(db.String(20))
     
-    def __init__(self, id, name, email, phone):
+    def __init__(self, id, name, type, company):
         self.id = id
         self.name = name
-        self.email = email
-        self.phone = phone
-        
-    # Staff Schema
+        self.type = type
+        self.company = company
+
+# Staff Schema
 class StaffSchema(ma.Schema):
     class Meta:
-        fields =('id', 'name', 'email', 'phone')
+        fields =('id', 'name', 'type', 'company')
 
 # Init Schema 
 staff_schema = StaffSchema()
 staffs_schema = StaffSchema(many=True)
 
 # Get All Staffs
-@app.route('/staffs', methods=['GET'])
+@app.route('/KPOPs', methods=['GET'])
 def get_staffs():
-    all_staffs = Staffs.query.all()
+    all_staffs = KPOPs.query.all()
     result = staffs_schema.dump(all_staffs)
     return jsonify(result)
-
+    
 # Get Single Staff
 @app.route('/staff/<id>', methods=['GET'])
 def get_staff(id):
